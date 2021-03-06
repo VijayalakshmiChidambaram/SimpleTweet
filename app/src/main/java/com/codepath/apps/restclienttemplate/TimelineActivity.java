68fccd96100f78d8ae567248d4dc98f9eddaee1e
VijayalakshmiChidambaram <vijayalakshmi1994.kc@gmail.com>
2021-03-05 21:43:46 -0600

    added a scrollListener to load more data

--- app/src/main/java/com/codepath/apps/restclienttemplate/TimelineActivity.java
+++ app/src/main/java/com/codepath/apps/restclienttemplate/TimelineActivity.java
@@ -27,6 +27,7 @@ public class TimelineActivity extends AppCompatActivity {
     List<Tweet> tweets;
     TweetsAdapter adapter;
     SwipeRefreshLayout swipeContainer;
+    EndlessRecyclerViewScrollListener scrollListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -56,12 +57,32 @@ public class TimelineActivity extends AppCompatActivity {
         //Initialize the list of tweets and adapter
         tweets = new ArrayList<>();
         adapter = new TweetsAdapter(this, tweets);
+        LinearLayoutManager layoutManager= new LinearLayoutManager(this);
         //Recycler View set up : 1) Layout Manager 2) Adapter
-        rvTweets.setLayoutManager(new LinearLayoutManager(this));
+        rvTweets.setLayoutManager(layoutManager);
         rvTweets.setAdapter(adapter);
+
+        scrollListener = new EndlessRecyclerViewScrollListener(layoutManager) {
+            @Override
+            public void onLoadMore(int page, int totalItemsCount, RecyclerView view) {
+            Log.i(TAG,"onLoadMore:" + page);
+            loadMoreData();
+            }
+        };
+        // Adds the scroll listener to RecyclerView
+        rvTweets.addOnScrollListener(scrollListener);
 
         //Populate Timeline feeds
         populateHomeTimeLine();
+    }
+
+    private void loadMoreData() {
+
+            // 1. Send an API request to retrieve appropriate paginated data
+            // 2. Deserialize and construct new model objects from the API response
+            // 3. Append the new data objects to the existing set of items inside the array of items
+            // 4. Notify the adapter of the new items made with `notifyItemRangeInserted()`
+
     }
 
     private void populateHomeTimeLine() {
