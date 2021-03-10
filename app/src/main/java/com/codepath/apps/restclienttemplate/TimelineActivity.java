85c84d9c570b910baae817d6731099843a6b865c
VijayalakshmiChidambaram <vijayalakshmi1994.kc@gmail.com>
2021-03-09 19:51:24 -0600

    Tweet compose screen set up

--- app/src/main/java/com/codepath/apps/restclienttemplate/TimelineActivity.java
+++ app/src/main/java/com/codepath/apps/restclienttemplate/TimelineActivity.java
@@ -5,6 +5,7 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
@@ -89,11 +90,17 @@ public class TimelineActivity extends AppCompatActivity {
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        if(item.getItemId() == R.id.compose)
+        if (item.getItemId() == R.id.compose) {
             //Compose item is selected
-            Toast.makeText(this,"Compose!", Toast.LENGTH_SHORT).show();
+            //Navigate to the new activity
+
+            Intent intent = new Intent(this, ComposeActivity.class);
+            startActivity(intent);
             return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
+
 
     private void loadMoreData() {
 
